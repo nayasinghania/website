29d3c79e131fe160e6a0d8de33ad97828507885f
Naya Singhania <74111380+nayasinghania@users.noreply.github.com>
2026-03-17 20:01:59 -0700

    feat: initial commit

--- components/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="max-w-md pb-16 text-sm text-slate-500 sm:pb-0">
-      <p>&copy; 2026 Naya Singhania</p>
-    </footer>
-  );
-}
--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "www.svgrepo.com/**",
-      },
-      {
-        protocol: "https",
-        hostname: "striking-kindness-e0d93214bb.media.strapiapp.com/**",
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
